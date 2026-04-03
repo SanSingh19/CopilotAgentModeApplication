df4891dacf592ce7e3d968f18ffc15bc3ec17f9e
Sanyukta <sanyuktakumari@gmail.com>
2026-04-03 18:08:38 +0000

    added URL endpoint

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-%c*i@303%ybv*4hj#g!=c5ct5e*a8bqjnnv7q)g585s)lokp@6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f"{codespace_name}-8000.app.github.dev")
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
